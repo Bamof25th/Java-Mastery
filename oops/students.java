396f66a53513478aeeeb495e5e591a24ed75a089
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-03-24 00:12:32 +0530

    copy constructors

--- oops/students.java
+++ oops/students.java
@@ -7,31 +7,54 @@ public class students {
 
     public static void main(String[] args) {
         Student s1 = new Student();
-        Student s2 = new Student("aniket");
-        Student s3 = new Student(014);
-        System.out.println(s1.name);
+        s1.name = "aniket";
+        s1.rollNo = 263;
+        s1.password = "asdasdaasd";
+        s1.marks[0] = 90;
+        s1.marks[1] = 98;
+        s1.marks[2] = 100;
+
+        Student s2 = new Student(s1);// copy
+        s2.password = "xyz";
+        s1.marks[0] = 99;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(s2.marks[i]);
+        }
     }
 }
 
 class Student {
     String name;
     int rollNo;
+    String password;
+    int marks[];
+
+    // *copy constructor */
+    Student(Student s1) {
+        marks = new int[3];
+        this.name = s1.name;
+        this.rollNo = s1.rollNo;
+        this.marks = s1.marks;
+    }
 
     // * constructor
     Student() {
         // non - parameterized
+        marks = new int[3];
         System.out.println("Construtor is called");
 
     }
 
     Student(String name) {
         // parameterized
+        marks = new int[3];
         this.name = name;
 
     }
 
     Student(int rollNo) {
         // parameterized
+        marks = new int[3];
         this.rollNo = rollNo;
 
     }
