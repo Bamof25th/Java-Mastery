142539a2eb8ce14b0f8a21b8abe0aafef1d8be5f
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-03-29 17:01:05 +0530

    polymorphism

---
+++ oops/polymorphism.java
@@ -0,0 +1,28 @@
+package oops;
+
+public class polymorphism {
+    public static void main(String[] args) {
+        claculator cal = new claculator();
+
+        System.out.println(cal.sum(1,2));
+        System.out.println(cal.sum((float)1.5,(float)2.5));
+        System.out.println(cal.sum(1,2,3));
+    }
+}
+
+/**
+ * claclator
+ */
+class claculator {
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    float sum(float a, float b) {
+        return a + b;
+    }
+
+    int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+}
