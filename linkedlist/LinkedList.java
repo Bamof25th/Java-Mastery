7617692bd31c0612e6de2c2c7fd47c40b42516bd
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-04-19 14:17:15 +0530

    print ll

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -13,11 +13,12 @@ public class LinkedList {
             this.data = data;
             this.next = null;
         }
+    }
 
         public static Node head;
         public static Node tail;
 
-        public void  addFirst(int data) {
+        public void addFirst(int data) {
 
             // step 1 = create new node
             Node newNode = new Node(data);
@@ -27,20 +28,48 @@ public class LinkedList {
                 head = tail = newNode;
                 return;
             }
-
+            
             // step2 = newNode next = head
             newNode.next = head;
             // step3 = head = newnode
             head = newNode;
         }
-
-        public static void main(String[] args) {
-            LinkedList ll = new LinkedList();
-            // *main function code
-            // ll.head = new Node(1);
-            // ll.head.next = new Node(2);
-            ll.addFirst(1);
-            ll.addFirst(2);
+        public void addLast(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = tail = newNode;
+            }
+            tail.next = newNode;
+            tail = newNode;
         }
+
+        public void print() {
+            Node temp = head;
+            if (head == null) {
+                System.out.println("empty");
+            }
+            while (temp != null) {
+                System.out.print(temp.data + "->");
+                temp = temp.next;
+
+            }
+            System.out.println("null");
+        }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        // *main function code
+        // ll.head = new Node(1);
+        // ll.head.next = new Node(2);
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
     }
 }
+
