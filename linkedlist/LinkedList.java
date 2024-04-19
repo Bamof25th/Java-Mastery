23c8fccdd49609f7b16df793bb0108eb4429557c
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-04-19 14:28:57 +0530

    add in middle if the linked list

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -15,46 +15,60 @@ public class LinkedList {
         }
     }
 
-        public static Node head;
-        public static Node tail;
+    public static Node head;
+    public static Node tail;
 
-        public void addFirst(int data) {
+    public void addFirst(int data) {
 
-            // step 1 = create new node
-            Node newNode = new Node(data);
+        // step 1 = create new node
+        Node newNode = new Node(data);
 
-            // exception if ll is empty
-            if (head == null) {
-                head = tail = newNode;
-                return;
-            }
-            
-            // step2 = newNode next = head
-            newNode.next = head;
-            // step3 = head = newnode
-            head = newNode;
-        }
-        public void addLast(int data) {
-            Node newNode = new Node(data);
-            if (head == null) {
-                head = tail = newNode;
-            }
-            tail.next = newNode;
-            tail = newNode;
+        // exception if ll is empty
+        if (head == null) {
+            head = tail = newNode;
+            return;
         }
 
-        public void print() {
-            Node temp = head;
-            if (head == null) {
-                System.out.println("empty");
-            }
-            while (temp != null) {
-                System.out.print(temp.data + "->");
-                temp = temp.next;
+        // step2 = newNode next = head
+        newNode.next = head;
+        // step3 = head = newnode
+        head = newNode;
+    }
 
-            }
-            System.out.println("null");
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
         }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void add(int idx, int data) {
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while (i != idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+    }
+
+    public void print() {
+        Node temp = head;
+        if (head == null) {
+            System.out.println("empty");
+        }
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+
+        }
+        System.out.println("null");
+    }
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
@@ -69,7 +83,7 @@ public class LinkedList {
         ll.addLast(3);
         ll.print();
         ll.addLast(4);
+        ll.add(2,99);
         ll.print();
     }
 }
-
