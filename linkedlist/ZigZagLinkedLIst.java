30c52f2f5cb5a163fdc1cff712a344b58aefdf34
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-04-25 19:12:43 +0530

    ZigZagLinkedLIst Done

---
+++ linkedlist/ZigZagLinkedLIst.java
@@ -0,0 +1,59 @@
+package linkedlist;
+
+import linkedlist.LinkedList.Node;
+
+public class ZigZagLinkedLIst {
+    public static void convertZigzag(Node head) {
+        // find niddle
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node mid = slow;
+        // reverse second half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+        }
+
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        // altMerg - zigzag merge
+
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.print();
+        convertZigzag(ll.head);
+        ll.print();
+    }
+}
