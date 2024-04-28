8249f9f84716e87a8ff29c4f154f1d1dfe710818
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-04-28 19:45:45 +0530

    stack with linked list

--- stack/StackB.java
+++ stack/StackB.java
@@ -3,51 +3,103 @@ package stack;
 import java.util.ArrayList;
 
 public class StackB {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
     static class Stack {
-       
+        static Node head = null;
+
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        public static void push(int data) {
+            Node newNode = new Node(data);
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return head.data;
+        }
+
+        public static void printStack() {
+            System.out.println("The stack elements are:");
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
-        Stack s= new Stack();
+        Stack s = new Stack();
 
         s.push(1);
         s.push(2);
         s.push(3);
 
-        while(!s.isEmpty()){
+        while (!s.isEmpty()) {
             System.out.println(s.peek());
-            s.pop(); 
+            s.pop();
         }
         System.out.println(s.peek());
     }
 }
 
-
 // static ArrayList<Integer> list = new ArrayList<>();
 
-//     public static boolean isEmpty() {
-//         return list.size() == 0;
-//     }
+// public static boolean isEmpty() {
+// return list.size() == 0;
+// }
 
-//     // *push
-//     public static void push(int data) {
-//         list.add(data);
-//     }
+// // *push
+// public static void push(int data) {
+// list.add(data);
+// }
 
-//     // *pop
-//     public static int pop() {
-//         if(isEmpty()){
-//             return -1;
-//         }
-//         int top = list.get(list.size() - 1);
-//         list.remove(list.size() - 1);
-//         return top;
-//     }
+// // *pop
+// public static int pop() {
+// if(isEmpty()){
+// return -1;
+// }
+// int top = list.get(list.size() - 1);
+// list.remove(list.size() - 1);
+// return top;
+// }
 
-//     // *peek
-//     public static int peek() {
-//         if(isEmpty()){
-//             return -1;
-//         }
-//         return list.get(list.size() - 1);
-//     }
+// // *peek
+// public static int peek() {
+// if(isEmpty()){
+// return -1;
+// }
+// return list.get(list.size() - 1);
+// }
 // }
