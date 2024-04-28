98778732389092e9b7c90f66995c4084d2a579ac
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-04-28 19:52:08 +0530

    stack usin utils

---
+++ stack/StackC.java
@@ -0,0 +1,21 @@
+package stack;
+
+import java.util.Stack;
+
+public class StackC {
+
+    public static void main(String[] args) {
+        Stack <Integer> s  = new Stack<>(); 
+        
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+        // System.out.println(s.peek());
+
+    }
+}
