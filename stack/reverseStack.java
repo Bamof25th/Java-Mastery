2cb52f68a50e45f27264b7031cef3acba4e9efa1
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-07-03 00:17:41 +0530

    stockspan done

--- stack/reverseStack.java
+++ stack/reverseStack.java
@@ -3,6 +3,33 @@ package stack;
 import java.util.Stack;
 
 public class reverseStack {
+    public static void pushAtBottom(Stack<Integer> s, int n) {
+        if (s.isEmpty()) {
+            s.push(n);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, n);
+        s.push(top);
+    }
+
+    public static void reverser(Stack<Integer> st) {
+        if (st.isEmpty()) {
+
+            return;
+        }
+        int top = st.pop();
+        reverser(st);
+        pushAtBottom(st, top);
+    }
+
+    public static void print(Stack<Integer> st) {
+        while (!st.isEmpty()) {
+            System.out.println(st.pop());
+        }
+
+    }
+
     public static void main(String[] args) {
         Stack<Integer> st = new Stack<>();
         st.push(1);
@@ -10,6 +37,8 @@ public class reverseStack {
         st.push(3);
         st.push(4);
         st.push(5);
+
+        reverser(st);
+        print(st);
     }
 }
- 
---
+++ stack/stockspan.java
@@ -0,0 +1,35 @@
+package stack;
+
+import java.util.Stack;
+
+public class stockspan {
+
+    public static void getStockSpan(int stock[], int span[]) {
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+        for (int i = 1; i < stock.length; i++) {
+            int currPrice = stock[i];
+            while (!s.isEmpty() && currPrice > stock[s.peek()]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                span[i] = i + 1;
+            } else {
+                int prevhigh = s.peek();
+                span[i] = i - prevhigh;
+            }
+            s.push(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int stocks[] = { 100, 80, 60, 70, 60, 85, 100 };
+        int span[] = new int[stocks.length];
+        getStockSpan(stocks, span);
+
+        for (int i = 0; i < span.length; i++) {
+            System.out.print(span[i] + ",");
+        }
+    }
+}
