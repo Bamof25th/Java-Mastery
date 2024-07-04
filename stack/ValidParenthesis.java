a8781176142fa9af9fe0cbde345091adc83a7d42
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-07-04 23:36:07 +0530

    valid parenthesis done

---
+++ stack/ValidParenthesis.java
@@ -0,0 +1,33 @@
+package stack;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                char clb = st.peek();
+                if ((clb == '(' && ch == ')') || (clb == '[' && ch == ']') || (clb == '{' && ch == '}')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String st = "({{[]}}()";
+        boolean a = isValid(st);
+
+        System.out.println(a);
+    }
+}
